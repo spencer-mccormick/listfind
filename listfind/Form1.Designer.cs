3ad72b5968694042959dac4d64eb210fedbcc0ac
Spencer McCormick <spencer@contactspencer.com>
2019-07-20 18:44:31 -0600

    Initial commit

---
+++ listfind/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace listfind
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(3, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(749, 117);
+            this.panel1.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(132, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(108, 37);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "label3";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(375, 41);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(371, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Initialize";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(372, 70);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(377, 44);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Next >>";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(4, 70);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(362, 44);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(661, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "label2";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(4, 120);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(745, 550);
+            this.listBox1.TabIndex = 2;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.ListBox1_SelectedIndexChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(370, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(76, 25);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "label4";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(749, 671);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "listfind";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ listfind/Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+
+namespace listfind
+{
+    public partial class Form1 : Form
+    {
+
+        List<string> words = new List<string>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Text files|*.txt|Comma-seperated values|*.csv|All files|*.*";
+
+            ofd.ShowDialog();
+
+            if (!string.IsNullOrEmpty(ofd.FileName))
+            {
+                words = import(File.ReadAllLines(ofd.FileName));
+            }
+        }
+
+        List<string> import(string[] lines)
+        {
+            List<string> wordlist = new List<string>();
+
+            foreach (string line in lines)
+            {
+                wordlist.Add(line);
+            }
+
+            return wordlist;
+        }
+
+        Random rand = new Random();
+        Timer T;
+        int round_points = 0;
+        int total_points;
+
+        int StartRound()
+        {
+            const int max = 15;
+
+            listBox1.Items.Clear();
+            listBox1.ClearSelected();
+            listBox1.Items.AddRange(GetWords(max));
+
+            int rwi = rand.Next(max);
+            string realword = words[rand.Next(words.Count)];
+
+            label3.Text = realword;
+            listBox1.Items.Insert(rwi, realword);
+
+            return rwi;
+        }
+
+        string[] GetWords(int n)
+        {
+            List<string> rw = new List<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                rw.Add(words[rand.Next(words.Count)]);
+            }
+
+            return rw.ToArray();
+        }
+
+        int answer;
+        long round = 0;
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            label4.Text = "Round " + round.ToString();
+            round++;
+            answer = StartRound();
+            round_points = 40;
+            T.Start();
+        }
+
+        private void T_Tick(object sender, EventArgs e)
+        {
+            if (round_points > 0)
+            {
+                round_points--;
+            }
+
+            label1.Text = round_points.ToString();
+        }
+
+        private void ListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex == answer)
+            {
+                total_points += round_points;
+                MessageBox.Show("This is correct!", "Correct", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                T.Stop();
+            }
+            else
+            {
+                MessageBox.Show("This is incorrect!", "Incorrect", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                round_points = 0;
+                T.Stop();
+            }
+
+
+            label2.Text = total_points.ToString();
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            T = new Timer();
+            T.Tick += T_Tick;
+            T.Interval = 100;
+        }
+    }
+}
